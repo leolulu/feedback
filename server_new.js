a8f6be2731881ef7504575194e3f2fb13b5cfab2
leolulu <348699103@qq.com>
2018-05-31 19:15:50 +0800

    在线模板引擎

--- server_new.js
+++ server_new.js
@@ -1,8 +1,22 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express();
 
-app.get('',function(req,res){
-	res.send(req)
+app.engine('html',require('express-art-template'))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+// app.get('/tpl_add',function(req,res){
+app.get('/add',function(req,res){
+	res.render('tpl_add.html')
+})
+
+app.post('/tpl_show',function(req,res){
+	console.log(  req.body )
+	console.log(  req.body.original_text[1] )
+
+	console.log(Date() + ' 进行了一次转换');
+	res.render('tpl_show.html', req.body )
 })
 
 app.listen(3000,function(){
